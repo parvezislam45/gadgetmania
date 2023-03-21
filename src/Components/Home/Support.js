996ba126e5f2b8225e01e23ad672a3fba15ccb72
Parvez Islam <parvezislam45@gmail.com>
2023-03-22 00:19:25 +0600

    sytem change

--- src/Components/Home/Support.js
+++ src/Components/Home/Support.js
@@ -4,8 +4,8 @@ const Support = () => {
   return (
     <div className="container mx-auto grid grid-cols-2 md:grid-cols-4 gap-10 mt-20 gap-y-15">
       <div class="card card-side">
-        <img
-          src="https://i.ibb.co/9v6KcJk/delivery-service-via-modern-technology-smart-phone-app-92801491.jpg"
+        <img className="w-40 h-28"
+          src="https://www.seekpng.com/png/detail/172-1720811_get-your-15-off-coupon-free-shipping-logo.png"
           alt="Movie"
         />
         <div class="card-body">
@@ -13,8 +13,8 @@ const Support = () => {
         </div>
       </div>
       <div class="card card-side">
-        <img
-          src="https://i.ibb.co/f1KjQ6x/Featured-Image-Money-Back-Guarantee.jpg"
+        <img className="w-40 h-28"
+          src="https://img.freepik.com/premium-vector/easy-returns-sign-label-delivery-service-vector-stock-illustration_100456-11337.jpg"
           alt="Movie"
         />
         <div class="card-body">
@@ -22,13 +22,13 @@ const Support = () => {
         </div>
       </div>
       <div class="card card-side">
-        <img src="https://i.ibb.co/rcjpbhG/secure-payments.png" alt="Movie" />
+        <img className="w-40 h-28" src="https://img.freepik.com/premium-vector/secure-payment-with-mobile-banking-application_258153-435.jpg" alt="Movie" />
         <div class="card-body">
           <p>100% Secure Payment</p>
         </div>
       </div>
       <div class="card card-side">
-        <img src="https://i.ibb.co/6FcPzzV/support.jpg" alt="Movie" />
+        <img className="w-40 h-28" src="https://img.freepik.com/premium-vector/contact-us-customer-service-illustration_2175-309.jpg" alt="Movie" />
         <div class="card-body">
           <p>Customer Support</p>
         </div>
